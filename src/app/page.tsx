f95c8c34b940222be462b09ee14c544617165bce
kristoffer <kunkristoffer@gmail.com>
2025-08-16 21:26:45 +0200

    implemented new landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import { LandingContact } from '@/components/layout/landingPage/contact';
+import { LandingDonate } from '@/components/layout/landingPage/donate';
+import { LandingGallery } from '@/components/layout/landingPage/gallery';
+import { LandingHero } from '@/components/layout/landingPage/hero';
+import { LandingJourney } from '@/components/layout/landingPage/journey';
+import { LandingMission } from '@/components/layout/landingPage/mission';
+import { LandingProjects } from '@/components/layout/landingPage/projects';
+import { LandingSponsors } from '@/components/layout/landingPage/sponsors';
+import { LandingVolunteer } from '@/components/layout/landingPage/volunteer';
+
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <main className=''>
+      <LandingHero />
+      <LandingMission />
+      <LandingProjects />
+      <LandingVolunteer />
+      <LandingJourney />
+      <LandingGallery />
+      <LandingDonate />
+      <LandingSponsors />
+      <LandingContact />
+    </main>
+  );
 }
