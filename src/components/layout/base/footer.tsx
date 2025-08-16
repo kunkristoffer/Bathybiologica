8c6bbc616fc5ddafa903e5df813ab48b1d912ae1
kristoffer <kunkristoffer@gmail.com>
2025-08-16 21:09:54 +0200

    created placeholder footer component

---
+++ src/components/layout/base/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className='bg-sky-950'>
+      <div className=''>
+        <p>footer</p>
+        <p>footer</p>
+      </div>
+    </footer>
+  );
+}
