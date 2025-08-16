9937bb762844f2313fae9c23dd5ddb910fbfa77a
kristoffer <kunkristoffer@gmail.com>
2025-08-16 21:10:04 +0200

    added navigation components to header component

---
+++ src/components/layout/base/header.tsx
@@ -0,0 +1,21 @@
+import { ButtonLink } from '@/components/ui/buttons/buttonLink';
+import { NavigationDesktop } from '@/components/layout/navigation/desktop';
+import { NavigationMobile } from '@/components/layout/navigation/mobile';
+
+export function Header() {
+  return (
+    <header className='w-full fixed h-20'>
+      <div className=''>
+        <span className='flex-1'>
+          <ButtonLink href='/#' label='logo' />
+        </span>
+        <NavigationDesktop />
+        <span className='flex-1 flex justify-end-safe items-center gap-2'>
+          <ButtonLink href='/#' label='🌑' variant='secondary' style='outline' />
+          <ButtonLink href='/#' label='Donate Now' variant='secondary' className='max-lg:hidden' />
+          <NavigationMobile />
+        </span>
+      </div>
+    </header>
+  );
+}
