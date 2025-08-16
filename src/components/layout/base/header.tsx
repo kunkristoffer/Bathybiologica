e93b2294d12d422fd7043d316a2589e0af652445
kristoffer <kunkristoffer@gmail.com>
2025-08-16 21:42:20 +0200

    fixed isolation context affecting mobile menu

--- src/components/layout/base/header.tsx
+++ src/components/layout/base/header.tsx
@@ -1,18 +1,18 @@
-import { ButtonLink } from '@/components/ui/buttons/buttonLink';
-import { NavigationDesktop } from '@/components/layout/navigation/desktop';
-import { NavigationMobile } from '@/components/layout/navigation/mobile';
+import { ButtonLink } from "@/components/ui/buttons/buttonLink";
+import { NavigationDesktop } from "@/components/layout/navigation/desktop";
+import { NavigationMobile } from "@/components/layout/navigation/mobile";
 
 export function Header() {
   return (
-    <header className='w-full fixed h-20'>
-      <div className=''>
-        <span className='flex-1'>
-          <ButtonLink href='/#' label='logo' />
+    <header className="w-full fixed">
+      <div>
+        <span className="flex-1">
+          <ButtonLink href="/#" label="logo" />
         </span>
         <NavigationDesktop />
-        <span className='flex-1 flex justify-end-safe items-center gap-2'>
-          <ButtonLink href='/#' label='🌑' variant='secondary' style='outline' />
-          <ButtonLink href='/#' label='Donate Now' variant='secondary' className='max-lg:hidden' />
+        <span className="flex-1 flex justify-end-safe items-center gap-2">
+          <ButtonLink href="/#" label="🌑" variant="secondary" style="outline" />
+          <ButtonLink href="/#" label="Donate Now" variant="secondary" className="max-lg:hidden" />
           <NavigationMobile />
         </span>
       </div>
--- src/components/layout/navigation/mobile.tsx
+++ src/components/layout/navigation/mobile.tsx
@@ -1,28 +1,28 @@
-'use client';
+"use client";
 
-import menuLinks from '@/data/navigation/links.json';
-import Link from 'next/link';
-import { useRef, useState } from 'react';
-import { useClickOutside } from '@/hooks/useClickOutside';
+import menuLinks from "@/data/navigation/links.json";
+import Link from "next/link";
+import { useRef, useState } from "react";
+import { useClickOutside } from "@/hooks/useClickOutside";
 
 export function NavigationMobile() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const containerRef = useRef<HTMLElement>(null);
   useClickOutside(containerRef, () => setIsOpen(false));
 
   return (
-    <nav ref={containerRef} className='sm:hidden'>
+    <nav ref={containerRef} className="sm:hidden">
       <button onClick={() => setIsOpen(!isOpen)}>🍞</button>
       <ul
         className={`
-          fixed inset-0 top-20
+          z-40 fixed inset-0 top-20
           flex flex-col items-center justify-center gap-4 p-4
-          ${isOpen ? '' : 'translate-x-full'}
+          ${isOpen ? "" : "translate-x-full"}
           backdrop-blur-md backdrop-brightness-50 transition-transform
         `}
       >
         {menuLinks.map((item) => (
-          <li key={'navbar-desktop-' + item.label}>
+          <li key={"navbar-desktop-" + item.label}>
             <Link href={item.href}>{item.label}</Link>
           </li>
         ))}
