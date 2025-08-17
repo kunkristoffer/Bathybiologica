490df6dbe5d3719930849097cd467ac04dece5cf
kristoffer <kunkristoffer@gmail.com>
2025-08-17 20:44:26 +0200

    added more fields to text input components

--- src/components/ui/input/text.tsx
+++ src/components/ui/input/text.tsx
@@ -1,23 +1,36 @@
 interface FormInputTextProps {
   label: string;
   name: string;
+  /** Default value */
+  defaultValue?: string;
   /** Placeholder text */
   placeholder?: string;
   /** Is the field required? */
   required?: boolean;
+  /** Error message for user */
+  errorMessage?: string;
 }
 
-export function FormInputText({ label, name, placeholder, required = false }: FormInputTextProps) {
+export function FormInputText({
+  label,
+  name,
+  defaultValue,
+  placeholder,
+  required = false,
+  errorMessage,
+}: FormInputTextProps) {
   return (
     <label htmlFor='' className='flex-1 flex flex-col'>
       <p>{label}</p>
       <input
         type='text'
         name={name}
+        defaultValue={defaultValue}
         placeholder={placeholder}
         required={required}
         className='bg-input-field rounded-md p-2'
       />
+      {errorMessage && <p className='text-error'>{errorMessage}</p>}
     </label>
   );
 }
--- src/components/ui/input/textArea.tsx
+++ src/components/ui/input/textArea.tsx
@@ -1,19 +1,37 @@
 interface FormInputTextAreaProps {
   label: string;
   name: string;
-  /** Optional limit on lines displayed before scroll takes over */
-  lines?: number;
+  /** Default value */
+  defaultValue?: string;
   /** Placeholder text */
   placeholder?: string;
   /** Is the field required? */
   required?: boolean;
+  /** Error message for user */
+  errorMessage?: string;
+  /** Optional limit on lines displayed before scroll takes over */
+  lines?: number;
 }
 
-export function FormInputTextArea({ label, name, lines = 5, placeholder, required = false }: FormInputTextAreaProps) {
+export function FormInputTextArea({
+  label,
+  name,
+  defaultValue,
+  placeholder,
+  required = false,
+  errorMessage,
+}: FormInputTextAreaProps) {
   return (
     <label htmlFor='' className='flex flex-col'>
       <p>{label}</p>
-      <textarea name={name} placeholder={placeholder} required={required} className={`bg-input-field rounded-md p-2`} />
+      <textarea
+        name={name}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        required={required}
+        className={`bg-input-field rounded-md p-2`}
+      />
+      {errorMessage && <p className='text-error'>{errorMessage}</p>}
     </label>
   );
 }
