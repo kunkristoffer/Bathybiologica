94ae1e725a34193c3241c6fccd1dbb95e3f990e4
kristoffer <kunkristoffer@gmail.com>
2025-08-17 15:43:00 +0200

    added history page placeholder

---
+++ src/app/history/page.tsx
@@ -0,0 +1,12 @@
+import { Section } from "@/components/layout/base/section";
+
+export default function About() {
+  return (
+    <main>
+      <Section>
+        <h1>history</h1>
+        <p>blabla</p>
+      </Section>
+    </main>
+  );
+}
