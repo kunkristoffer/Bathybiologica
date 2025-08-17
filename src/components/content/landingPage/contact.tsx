5c6e785ac78b3530010d69ebfe15b9c413da9156
kristoffer <kunkristoffer@gmail.com>
2025-08-17 18:58:48 +0200

    added new contact form component to contact us section

--- src/components/content/landingPage/contact.tsx
+++ src/components/content/landingPage/contact.tsx
@@ -1,9 +1,14 @@
+import { ContactUsForm } from '@/components/forms/contactUs';
 import { Section } from '@/components/layout/base/section';
 
 export function LandingContact() {
   return (
-    <Section id='contact' sectionClassName='' className=''>
-      <p>Contact</p>
+    <Section id='contact' sectionClassName='' className='flex-row gap-12'>
+      <div className='flex-1 flex flex-col gap-4'>
+        <h2>Contact us</h2>
+        <p>Have questions about our work or how you can get involved? We&apos;d love to hear from you.</p>
+      </div>
+      <ContactUsForm className='flex-1' />
     </Section>
   );
 }
