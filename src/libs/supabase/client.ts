4035ba513940911fdbb69625ad36f43bdddbdc45
Kristoffer <kunkristoffer@gmail.com>
2026-02-14 19:06:22 +0100

    Added functions for storing contact requests through supabase database

---
+++ src/libs/supabase/client.ts
@@ -0,0 +1,6 @@
+import { Database } from '@/types/supabase.types'
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!)
+
+
