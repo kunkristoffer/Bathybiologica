e1b153a62cdfdf504d74b76b892281dee5297fc9
Kristoffer <kunkristoffer@gmail.com>
2026-02-14 19:38:49 +0100

    added temporary text disclaimer on subpages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,11 @@
-import { Section } from "@/components/layout/base/section";
+import { Section } from '@/components/layout/base/section';
 
 export default function About() {
   return (
     <main>
       <Section>
         <h1>About</h1>
-        <p>blabla</p>
+        <p>This section is under development, check back later</p>
       </Section>
     </main>
   );
