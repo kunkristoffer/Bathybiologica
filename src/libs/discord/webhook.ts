ec889bd5902182abfddfb41ce98ac679fc80a5c8
Kristoffer <kunkristoffer@gmail.com>
2026-02-15 19:49:50 +0100

    enabling logging for testing webhook issue in vercel deployment

--- src/libs/discord/webhook.ts
+++ src/libs/discord/webhook.ts
@@ -47,6 +47,8 @@ export class DiscordHook {
         embeds: this.embeds,
       }
 
+      console.log(payload)
+
       // Send message
       const response = await fetch(this.webhookUrl, {
         method: "POST",
