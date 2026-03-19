76888d4463efeb47cf678206aecc7af20197ca33
Kristoffer <kunkristoffer@gmail.com>
2026-03-19 23:24:30 +0100

    Updated theme switcher server action to no longer require form action and renamed the theme type export

--- src/actions/changeTheme.ts
+++ src/actions/changeTheme.ts
@@ -1,14 +1,9 @@
 "use server";
 
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
-export type Theme = "light" | "dark" | "system";
+export type ThemeOptions = "light" | "dark" | "system";
 
-export async function changeTheme(formData: FormData) {
-  const theme = (formData.get("theme") as Theme) ?? "system";
-  const path = String(formData.get("path") || "/");
-
-  (await cookies()).set("theme", theme, { path: "/", maxAge: 60 * 60 * 24 * 365 });
-  redirect(path);
+export async function changeTheme(newTheme: ThemeOptions) {
+  (await cookies()).set("theme", newTheme, { maxAge: 60 * 60 * 24 * 365 });
 }
