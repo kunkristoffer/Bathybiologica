f297715ff8b166e99a9f7d52d51705cbf5e8696f
Kristoffer <kunkristoffer@gmail.com>
2026-03-24 13:18:08 +0100

    Modified how env variables are loaded to avoid exposing secrets during test results

--- src/libs/discord/actions.test.ts
+++ src/libs/discord/actions.test.ts
@@ -1,29 +1,35 @@
+import type { DiscordEmbeds, DiscordEmbedsField } from '@/types/discord.types';
+import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest'
+import { http, HttpResponse } from "msw"
+import { setupServer } from "msw/node"
 import { DiscordHook } from '@/libs/discord/webhook'
-import { DiscordEmbeds } from '@/types/discord.types';
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+const webhookUrl = process.env.TEST_DISCORD_WEBHOOK!
+const server = setupServer()
+
+// Start server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+
+// Close server after all tests
+afterAll(() => server.close())
+
+// Reset handlers after each test for test isolation
+afterEach(() => server.resetHandlers())
+
 
 describe("Discord webhook client", () => {
-  beforeEach(() => {
-    vi.restoreAllMocks();
-    vi.stubGlobal('fetch', vi.fn());
-  });
-
-  afterEach(() => {
-    vi.unstubAllGlobals();
-  });
-
   it("Throws when constructed without url", () => {
     expect(() => new DiscordHook("")).toThrow("DiscordHook class called without a webhook url")
   })
 
   it("Supports adding message and embeds", () => {
-    const hook = new DiscordHook("url")
+    const hook = new DiscordHook(webhookUrl)
 
     const message = "Test message"
     hook.message(message)
     expect(hook.body.content).toBe(message)
 
-    const embed = {
+    const embed: DiscordEmbeds = {
       title: 'Test submission',
       color: 12345,
       fields: [{ name: 'Test', value: 'Tester' }],
@@ -33,10 +39,10 @@ describe("Discord webhook client", () => {
   })
 
   it("Throws when adding more than 25 fields at a time", () => {
-    const hook = new DiscordHook("url")
+    const hook = new DiscordHook(webhookUrl)
 
-    const fields = Array.from(Array(26), (x) => ({ name: 'Test', value: 'Tester' }))
-    const embeds = {
+    const fields: DiscordEmbedsField[] = Array.from(Array(26), (x) => ({ name: 'Test', value: 'Tester' }))
+    const embeds: DiscordEmbeds = {
       title: 'Test submission',
       color: 12345,
       fields,
@@ -46,9 +52,9 @@ describe("Discord webhook client", () => {
   })
 
   it("Throws when adding more than 10 embeds", () => {
-    const hook = new DiscordHook("url")
+    const hook = new DiscordHook(webhookUrl)
 
-    const embed = {
+    const embed: DiscordEmbeds = {
       title: 'Test submission',
       color: 12345,
       fields: [{ name: "Test", value: "Message" }]
@@ -60,5 +66,9 @@ describe("Discord webhook client", () => {
     }
 
     expect(() => hook.embed(embed)).toThrow("Discord webhook messages can contain a maximum of 10 embeds")
+  })
+
+  it("Constructs and sends valid payload", () => {
+    const hook = new DiscordHook(webhookUrl)
   })
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
   },
+  envPrefix: "TEST_",
   resolve: {
     tsconfigPaths: true
   }
