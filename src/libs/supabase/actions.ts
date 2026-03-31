20a6eee396c0c3d52851e4c4ea1a05a205f55b25
Kristoffer <kunkristoffer@gmail.com>
2026-03-31 22:06:33 +0200

    Simplified dpPostContact by returning both data and error

--- src/libs/supabase/actions.ts
+++ src/libs/supabase/actions.ts
@@ -4,27 +4,15 @@ import { supabaseAdmin } from "@/libs/supabase/client"
 import { Contact } from "@/validation/contactForm"
 
 export async function dbPostContact(contact: Contact) {
-  try {
-    // Created a cleaned version
-    const contactCleaned: Omit<Contact, "hp" | "consent"> = {
-      first_name: contact.first_name,
-      last_name: contact.last_name,
-      email: contact.email,
-      subject: contact.subject,
-      message: contact.message
-    }
-
-    // Attempt db insert, id and created_at will be filled automatically
-    const { data, error } = await supabaseAdmin.from("contacts").insert(contactCleaned).select()
-
-    // Propagate errors to caller
-    if (error) {
-      throw error
-    }
-
-    // Return data in case we want to check id/timestamp
-    return data.length ? data.at(0) : undefined
-  } catch (err) {
-    throw err
+  // Created a cleaned version of contact
+  const contactCleaned: Omit<Contact, "hp" | "consent"> = {
+    first_name: contact.first_name,
+    last_name: contact.last_name,
+    email: contact.email,
+    subject: contact.subject,
+    message: contact.message
   }
+
+  // Attempt db insert, id and created_at will be filled automatically
+  return await supabaseAdmin.from("contacts").insert(contactCleaned).select().single()
 }
