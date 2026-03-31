d70c9efa32255ac4d2c45103a2f80c4b0a1abf87
Kristoffer <kunkristoffer@gmail.com>
2026-03-31 19:20:38 +0200

    Added placeholder privacy page for avoiding stale links

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,12 @@
+import { Section } from '@/components/layout/base/section';
+
+export default function Privacy() {
+  return (
+    <main>
+      <Section>
+        <h1>Privacy notice</h1>
+        <p>This section is under development, check back later</p>
+      </Section>
+    </main>
+  );
+}
