f43c575a21566f8a44d8cd652c6d53a7b5e4c2ab
Kristoffer <kunkristoffer@gmail.com>
2026-04-02 18:56:29 +0200

    Added global css class that can prevent document level scrolling, useful for modals

--- src/components/layout/navigation/mobile.tsx
+++ src/components/layout/navigation/mobile.tsx
@@ -1,9 +1,11 @@
-"use client";
+'use client';
 
-import menuLinks from "@/data/navigation/links.json";
-import Link from "next/link";
-import { useRef, useState } from "react";
-import { useClickOutside } from "@/hooks/useClickOutside";
+import menuLinks from '@/data/navigation/links.json';
+import Link from 'next/link';
+import { useRef, useState } from 'react';
+import { useClickOutside } from '@/hooks/useClickOutside';
+import { HamburgerIcon } from '@/components/ui/icons/Hamburger';
+import { Globe } from 'lucide-react';
 
 export function NavigationMobile() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,22 +13,31 @@ export function NavigationMobile() {
   useClickOutside(containerRef, () => setIsOpen(false));
 
   return (
-    <nav ref={containerRef} className="sm:hidden">
-      <button onClick={() => setIsOpen(!isOpen)}>🍞</button>
-      <ul
+    <nav ref={containerRef} className={`sm:hidden ${isOpen ? 'stop-scroll' : ''}`}>
+      <HamburgerIcon active={isOpen} onClick={() => setIsOpen(!isOpen)} />
+      <div
         className={`
-          fixed inset-0 top-20
-          flex flex-col items-center justify-center gap-4 p-4
-          ${isOpen ? "" : "translate-x-full"}
+          fixed inset-0 top-20 flex flex-col
+          ${isOpen ? '' : 'translate-x-full'}
           backdrop-blur-md backdrop-brightness-50 transition-transform
         `}
       >
-        {menuLinks.map((item) => (
-          <li key={"navbar-desktop-" + item.label}>
-            <Link href={item.href}>{item.label}</Link>
-          </li>
-        ))}
-      </ul>
+        <nav className='flex-1 flex flex-col items-center justify-center gap-4 p-4'>
+          {menuLinks.map((item) => (
+            <Link href={item.href} key={'navbar-desktop-' + item.label}>
+              {item.label}
+            </Link>
+          ))}
+        </nav>
+        <div className='bg-panel flex flex-col gap-4'>
+          <div>
+            <span className='flex gap-2'>
+              <Globe />
+              <h3>Language</h3>
+            </span>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
