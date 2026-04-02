e87df2dcf6263aa467bf18a13120cc459edd9110
Kristoffer <kunkristoffer@gmail.com>
2026-04-02 20:47:38 +0200

    Added the new mobile menu using the new mobile friendly menu components

--- src/components/layout/navigation/mobile.tsx
+++ src/components/layout/navigation/mobile.tsx
@@ -5,39 +5,46 @@ import Link from 'next/link';
 import { useRef, useState } from 'react';
 import { useClickOutside } from '@/hooks/useClickOutside';
 import { HamburgerIcon } from '@/components/ui/icons/Hamburger';
-import { Globe } from 'lucide-react';
+import { MobileMenuLanguageSelect } from '@/components/ui/menus/mobile/MenuLanguageSelect';
+import { MobileMenuAppearanceToggle } from '@/components/ui/menus/mobile/MenuAppearanceToggle';
+import { ThemeOptions } from '@/actions/changeTheme';
+import { useLocale } from 'next-intl';
 
-export function NavigationMobile() {
-  const [isOpen, setIsOpen] = useState(false);
-  const containerRef = useRef<HTMLElement>(null);
+export function NavigationMobile({ current }: { current: ThemeOptions }) {
+  const [isOpen, setIsOpen] = useState(true);
+  const containerRef = useRef<HTMLDivElement>(null);
   useClickOutside(containerRef, () => setIsOpen(false));
 
+  const locale = useLocale() as 'en' | 'no';
+
   return (
-    <nav ref={containerRef} className={`sm:hidden ${isOpen ? 'stop-scroll' : ''}`}>
-      <HamburgerIcon active={isOpen} onClick={() => setIsOpen(!isOpen)} />
+    <div ref={containerRef} className={`sm:hidden ${isOpen ? 'stop-scroll' : ''}`}>
+      <HamburgerIcon active={isOpen} onClick={() => setIsOpen(!isOpen)} className='size-8' />
       <div
         className={`
           fixed inset-0 top-20 flex flex-col
           ${isOpen ? '' : 'translate-x-full'}
-          backdrop-blur-md backdrop-brightness-50 transition-transform
+          backdrop-blur-xl backdrop-brightness-40  transition-transform
+          overflow-y-auto
         `}
       >
         <nav className='flex-1 flex flex-col items-center justify-center gap-4 p-4'>
           {menuLinks.map((item) => (
-            <Link href={item.href} key={'navbar-desktop-' + item.label}>
-              {item.label}
+            <Link
+              href={item.href}
+              key={'navbar-desktop-' + item.label[locale]}
+              className='uppercase text-2xl/loose text-white'
+              onClick={() => setIsOpen(false)}
+            >
+              {item.label[locale]}
             </Link>
           ))}
         </nav>
-        <div className='bg-panel flex flex-col gap-4'>
-          <div>
-            <span className='flex gap-2'>
-              <Globe />
-              <h3>Language</h3>
-            </span>
-          </div>
+        <div className='bg-background flex flex-col gap-8 p-8'>
+          <MobileMenuLanguageSelect />
+          <MobileMenuAppearanceToggle current={current} />
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
