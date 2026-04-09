92a564de67f96976e51702741e8c5a2342c69755
Kristoffer <kunkristoffer@gmail.com>
2026-04-09 15:11:47 +0200

    Hoisted category expanded state, converted div element into details to use its native behaviour

--- src/components/content/cookieConsent/CookieCategory.tsx
+++ src/components/content/cookieConsent/CookieCategory.tsx
@@ -1,9 +1,11 @@
 import { FormCheckBox } from '@/components/ui/input/checkBox';
+import { CookieCategory, CookieOption, SelectedCookies } from '@/types/legal/cookieConsent.types';
 import { ListChevronsDownUp, ListChevronsUpDown } from 'lucide-react';
-import { useState } from 'react';
+import { useRef } from 'react';
 
 interface CookieConsentCategoryProps extends CookieCategory {
-  defaultOpen: boolean;
+  defaultOpen?: boolean;
+  selectedCookies: Record<string, boolean>;
 }
 
 export function CookieConsentCategory({
@@ -13,34 +15,38 @@ export function CookieConsentCategory({
   tooltip,
   options,
   defaultOpen = false,
+  selectedCookies,
 }: CookieConsentCategoryProps) {
-  const [isExpanded, setIsExpended] = useState(defaultOpen);
+  const detailsRef = useRef<HTMLDetailsElement>(null);
+  const selected = options.reduce((acc, cur) => (cur.isEnabled ? acc + 1 : 0), 0);
   return (
-    <div className='flex flex-col gap-2 panel'>
-      <span className='flex items-center gap-2 border-b border-panel-border pb-1'>
-        <FormCheckBox name={name} />
-        <h3 className='ml-2'>{label}</h3>
-        {tooltip && <small className='px-2 rounded-full bg-text-muted'>{tooltip}</small>}
-        <span className='ml-auto' onClick={() => setIsExpended((old) => !old)}>
-          {isExpanded ? <ListChevronsUpDown /> : <ListChevronsDownUp />}
+    <details ref={detailsRef} name='cookie-consent' className='flex flex-col gap-2 panel' open={defaultOpen}>
+      <summary className='flex flex-col'>
+        <span className='flex items-center gap-2 border-b border-panel-border pb-1'>
+          <FormCheckBox name={name} />
+          <h3 className='ml-2'>{label}</h3>
+          {detailsRef.current?.open}
+          {tooltip && <small className='px-2 rounded-full bg-text-muted'>{tooltip}</small>}
+          <small className='ml-auto text-text-muted'>{`${selected} of ${options.length} selected`}</small>
+          <span className=''>{detailsRef.current?.open ? <ListChevronsUpDown /> : <ListChevronsDownUp />}</span>
         </span>
-      </span>
-      <div className='flex flex-col gap-2'>
         <small className='italic py-2'>{description}</small>
-        {isExpanded &&
-          options.map((option) => (
-            <label
-              key={option.label + option.name}
-              className='flex flex-col justify-center p-2 gap-2 border rounded-md hover:bg-surface has-checked:bg-success/33'
-            >
-              <span className='flex gap-2'>
-                <p className='capitalize mr-auto'>{option.label}</p>
-                <FormCheckBox name={option.name} />
-              </span>
-              <small className=''>{option.description}</small>
-            </label>
-          ))}
+      </summary>
+      <div className='flex flex-col gap-2'>
+        {options.map((option) => (
+          <label
+            key={option.label + option.name}
+            className='flex flex-col justify-center p-2 gap-2 border rounded-md hover:bg-surface has-checked:bg-success/33'
+          >
+            <span className='flex gap-2'>
+              <p className='capitalize mr-auto'>{option.label}</p>
+              <p className=''>{selectedCookies[option.name]}</p>
+              <FormCheckBox name={option.name} />
+            </span>
+            <small className=''>{option.description}</small>
+          </label>
+        ))}
       </div>
-    </div>
+    </details>
   );
 }
