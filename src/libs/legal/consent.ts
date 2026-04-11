827c295f78e77553af1bdf9536f9915dad1bdc69
Kristoffer <kunkristoffer@gmail.com>
2026-04-12 00:49:42 +0200

    expanded has consent helper function with more keys

--- src/libs/legal/consent.ts
+++ src/libs/legal/consent.ts
@@ -3,16 +3,24 @@ import { getCookieConsent } from "@/actions/legal/cookieConsent";
 export async function hasConsent() {
   const cookie = await getCookieConsent()
 
-  if (!cookie) {
-    return {
-      showConsentDialog: true
-    }
+  // Cookie missing or malformed, show cookie consent
+  if (!cookie) return {
+    showConsentDialog: true
   }
 
-  const cookieDate = new Date(cookie.updatedAt)
-  const currDate = new Date()
 
+  const cookieDate = new Date(cookie.updatedAt).getTime()
+  const currDate = new Date().getTime()
+  const cookieMaxDays = 60
+
+  // Cookie died of old age, show cookie consent again sadge
+  if (currDate - cookieDate > (1000 * 60 * 60 * 24 * cookieMaxDays)) return {
+    showConsentDialog: true
+  }
+
+  // Cookie exist, expose details
   return {
-    showConsentDialog: false
+    showConsentDialog: false,
+    cookie
   }
 }
