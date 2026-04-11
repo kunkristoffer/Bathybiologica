9d57d46271f165e6c212a94bc3bb11ba045c9b79
Kristoffer <kunkristoffer@gmail.com>
2026-04-11 21:05:53 +0200

    Avoid breaking errors in cookie consent actions, instead log issues

--- src/actions/legal/cookieConsent.ts
+++ src/actions/legal/cookieConsent.ts
@@ -4,27 +4,37 @@ import type { ConsentCookie } from '@/types/legal/consent.types'
 import { cookies } from 'next/headers'
 
 export async function setCookieConsent() {
-  const cookieStore = await cookies()
+  try {
+    const cookieStore = await cookies()
 
-  const cookieValue: ConsentCookie = {
-    mode: "essential",
-    updatedAt: Date.now(),
-    version: 1
+    const cookieValue: ConsentCookie = {
+      mode: "all",
+      updatedAt: Date.now(),
+      version: 1
+    }
+
+    cookieStore.set({
+      name: 'consent',
+      value: JSON.stringify(cookieValue),
+    })
+  } catch (error) {
+    console.error(error)
   }
-
-  cookieStore.set({
-    name: 'consent',
-    value: JSON.stringify(cookieValue),
-  })
 }
 
 export async function getCookieConsent() {
-  const cookieStore = await cookies()
-  const cookie = cookieStore.get('consent')
+  try {
+    const cookieStore = await cookies()
+    const cookie = cookieStore.get('consent')
 
-  if (cookie) {
-    return JSON.parse(cookie.value) as ConsentCookie
+    if (cookie && cookie.value) {
+      const consentValue = JSON.parse(cookie.value) as ConsentCookie
+      return consentValue
+    }
+
+    return null
+  } catch (error) {
+    console.error(error)
+    return null
   }
-
-  return null
 }
