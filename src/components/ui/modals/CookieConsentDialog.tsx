ea946f442d19da4e6e833d2bcbd214e8deaca8f3
Kristoffer <kunkristoffer@gmail.com>
2026-04-14 23:58:28 +0200

    Lowered the cookie consent dialog z-index to 10 for letting users change locale

--- src/components/ui/modals/CookieConsentDialog.tsx
+++ src/components/ui/modals/CookieConsentDialog.tsx
@@ -6,7 +6,7 @@ export function CookieConsentDialog() {
       aria-labelledby='cookie-consent-title'
       closedby='none'
       className='
-        appearance-none z-50 fixed size-full flex justify-center items-end-safe
+        appearance-none z-10 fixed size-full flex justify-center items-end-safe
         bg-transparent backdrop-blur-xs backdrop-brightness-50
         stop-scroll
       '
