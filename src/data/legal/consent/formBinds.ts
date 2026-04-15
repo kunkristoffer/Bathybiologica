2bfe115a69d621696f08fe0ee7c57d887671f104
Kristoffer <kunkristoffer@gmail.com>
2026-04-15 20:53:48 +0200

    removed non-relevant cookie categories and options left from development

--- src/data/legal/consent/formBinds.ts
+++ src/data/legal/consent/formBinds.ts
@@ -2,41 +2,18 @@ import { ConsentCategory } from "@/types/legal/consent.types"
 
 export const consentFormData = [
   {
-    // Required for site to work propperly
     name: 'essential',
     isRequired: true,
-    tags: ["recommended"],
     options: [
       { name: "cookieConsent", isRequired: true, },
-      { name: "sessionCookie", isRequired: true, }
     ]
   }, {
-    // Used for site functionality, such as form submission, but is technically optional
     name: 'functional',
+    tags: ["essential"],
     options: [
-      { name: "recaptcha", tags: ["external", "recommended"] },
+      { name: "reCAPTCHA", tags: ["external"] },
       { name: "locale" },
       { name: "theme" },
-    ]
-  }, {
-    // Analyze how a user uses our site, only used for improving the site performance
-    name: 'analytics',
-    options: [
-      { name: "google", tags: ["external"] }
-    ]
-  }, {
-    // Allow social media integrations
-    name: 'social',
-    options: [
-      { name: "meta", tags: ["external"] }
-    ]
-  }, {
-    // Allow 3rd parties to collect metrics to improve SEO
-    name: 'campaigns',
-    options: [
-      { name: "google", tags: ["external"] },
-      { name: "youtube", tags: ["external"] },
-      { name: "meta", tags: ["external"] }
     ]
   }
 ] as const satisfies ConsentCategory[]
