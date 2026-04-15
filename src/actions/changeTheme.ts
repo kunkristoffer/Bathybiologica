6be5f2e09d578b0d0952288765b9d96ee306eac3
Kristoffer <kunkristoffer@gmail.com>
2026-04-15 02:34:03 +0200

    Added consent cookie check to both theme and language switcher action

--- src/actions/changeTheme.ts
+++ src/actions/changeTheme.ts
@@ -1,9 +1,17 @@
 "use server";
 
+import { checkConsent } from "@/libs/legal/consent";
 import { cookies } from "next/headers";
 
 export type ThemeOptions = "light" | "dark" | "system";
 
 export async function changeTheme(newTheme: ThemeOptions) {
-  (await cookies()).set("theme", newTheme, { maxAge: 60 * 60 * 24 * 365 });
+  const hasConsent = await checkConsent("theme")
+  if (!hasConsent) {
+    // Trigger consent error notification
+    return
+  }
+
+  const cookieStore = await cookies()
+  cookieStore.set("theme", newTheme, { maxAge: 60 * 60 * 24 * 365 });
 }
