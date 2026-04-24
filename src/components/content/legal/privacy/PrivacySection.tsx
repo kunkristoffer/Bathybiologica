505f136f6110c83a38dc452215bad820088822b6
Kristoffer <kunkristoffer@gmail.com>
2026-04-24 11:22:34 +0200

    Added style and dom improvements to privacy section to make it more organized

--- src/components/content/legal/privacy/PrivacySection.tsx
+++ src/components/content/legal/privacy/PrivacySection.tsx
@@ -6,23 +6,29 @@ import Link from 'next/link';
 // Components
 import { Section } from '@/components/layout/base/section';
 import { PrivacyBlock } from '@/components/content/legal/privacy/PrivacyBlock';
+import { Link as LinkIcon } from 'lucide-react';
 
 export async function PrivacySection({ id, index = [], level = 1, title, content, children }: PrivacySectionNode) {
   const Heading = `h${level}` as keyof JSX.IntrinsicElements;
   return (
-    <Section className='flex flex-col gap-8'>
-      <Link href={`#${id}`}>
-        <Heading id={id}>{`${index.join('.')} ${title}`}</Heading>
-      </Link>
-      <div className='flex flex-col gap-2'>
-        {content.map((item) => (
-          <PrivacyBlock key={id} {...item} />
-        ))}
-      </div>
+    <>
+      <Section className='group/sec flex flex-col gap-4'>
+        <Link href={`#${id}`} className='group/link flex items-center group-hover/sec:text-primary hover:underline'>
+          <Heading id={id} className={`${level == 2 ? 'border-b-2' : ''} font-bold`}>
+            {`${index.join('.')} ${title}`}
+          </Heading>
+          <LinkIcon className='hidden group-hover/link:block pl-2' />
+        </Link>
+        <div className='flex flex-col gap-2 p-2 border border-panel-border rounded-md'>
+          {content.map((item, i) => (
+            <PrivacyBlock key={`${item}-${i}`} {...item} />
+          ))}
+        </div>
+      </Section>
       {children &&
         children.map((child, i) => (
           <PrivacySection key={child.id} index={[...index, i + 1]} level={level + 1} {...child} />
         ))}
-    </Section>
+    </>
   );
 }
