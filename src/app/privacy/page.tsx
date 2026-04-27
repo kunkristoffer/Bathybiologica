8f7204cec166af9846b31214bfd5d2b7bef6d0ee
Kristoffer <kunkristoffer@gmail.com>
2026-04-27 13:17:52 +0200

    Added h5 to privacy page ToC levels

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -8,7 +8,7 @@ export default async function Privacy() {
   const privacy = messages.privacy as PrivacyMessages;
   return (
     <main className='relative sm:flex-row gap-4 p-4 md:px-8'>
-      <TableOfContents containerID='privacy-page' headingLevels={['h2', 'h3', 'h4']} />
+      <TableOfContents containerID='privacy-page' headingLevels={['h2', 'h3', 'h4', 'h5']} />
       <div id='privacy-page' className='flex flex-col gap-12'>
         {privacy.sections.map((data, i) => (
           <PrivacySection key={data.id} level={i === 0 ? 1 : 2} {...data} />
