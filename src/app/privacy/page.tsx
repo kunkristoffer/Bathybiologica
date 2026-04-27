67c6d1a0f4b744d190602bb0b0b2878505cd00f7
Kristoffer <kunkristoffer@gmail.com>
2026-04-27 14:13:51 +0200

    Added container size to privacy page to avoid page growing uncontrollably

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -7,7 +7,7 @@ export default async function Privacy() {
   const messages = await getMessages();
   const privacy = messages.privacy as PrivacyMessages;
   return (
-    <main className='relative sm:flex-row gap-4 p-4 md:px-8'>
+    <main className='container mx-auto relative sm:flex-row gap-4 p-4 md:px-8'>
       <TableOfContents containerID='privacy-page' headingLevels={['h2', 'h3', 'h4', 'h5']} />
       <div id='privacy-page' className='flex flex-col gap-12'>
         {privacy.sections.map((data, i) => (
