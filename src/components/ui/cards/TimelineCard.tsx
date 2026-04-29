ad16d6d30f8b104be3a96397405c61e40063b416
Kristoffer <kunkristoffer@gmail.com>
2026-04-29 14:52:49 +0200

    Minor style tweaks to timeline component for hovering states

--- src/components/ui/cards/TimelineCard.tsx
+++ src/components/ui/cards/TimelineCard.tsx
@@ -11,9 +11,14 @@ type TimelineCardProps = TimelineItem & ComponentProps<'div'> & { number?: numbe
 
 export function TimelineCard({ title, text, number, className }: TimelineCardProps) {
   return (
-    <div className={twMerge('relative border-l-2 border-tertiary pl-8 not-last:pb-8', className)}>
-      <span className='absolute top-0 left-0 -translate-x-1/2 inline-flex items-center justify-center size-8 rounded-full bg-tertiary border border-primary'>
-        <span>{number}</span>
+    <div
+      className={twMerge(
+        'group relative border-l-2 border-tertiary hover:border-primary ml-4 pl-8 not-last:pb-6 duration-200',
+        className
+      )}
+    >
+      <span className='absolute top-0 left-0 -translate-x-1/2 inline-flex items-center justify-center size-8 rounded-full bg-tertiary border-2 border-tertiary group-hover:border-primary duration-200'>
+        {number}
       </span>
       <h3>{title}</h3>
       <p>{text}</p>
