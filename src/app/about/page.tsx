8692ed306120aee7a7e6f50f75158479d39b5386
Kristoffer <kunkristoffer@gmail.com>
2026-04-30 13:38:13 +0200

    Added mockup of about us why section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,16 +4,21 @@ import { AboutHow } from '@/components/content/about/How';
 import { AboutMemory } from '@/components/content/about/Memory';
 import { AboutMission } from '@/components/content/about/Misson';
 import { AboutVision } from '@/components/content/about/Vision';
+import { AboutWhy } from '@/components/content/about/Why';
 
 export default function About() {
   return (
     <main className='gap-16'>
       <AboutHero />
+      <AboutWhy />
       <AboutMission />
       <AboutMemory />
       <AboutGoals />
       <AboutHow />
+      {/* impact/progress */}
       <AboutVision />
+      {/* get involved */}
+      {/* transparency */}
     </main>
   );
 }
---
+++ src/components/content/about/Why.tsx
@@ -0,0 +1,48 @@
+import { Section } from '@/components/layout/base/section';
+import { CardData, IconTitleTextCard } from '@/components/ui/cards/IconTitleText';
+import { Tagline } from '@/components/ui/tags/Tagline';
+import { Circle, Moon, Sun } from 'lucide-react';
+
+export function AboutWhy() {
+  const cardData: CardData[] = [
+    {
+      id: 'Limited Access',
+      title: 'Limited Access',
+      text: [
+        'Scientists and graduates often lack access to local research opportunities, forcing them to rely on expensive and complex expeditions.',
+      ],
+      icon: Sun,
+    },
+    {
+      id: 'High Barriers',
+      title: 'High Barriers',
+      text: [
+        'Traditional research expeditions involve significant costs, logistical complexity, and resource requirements that many cannot meet.',
+      ],
+      icon: Moon,
+    },
+    {
+      id: 'Missing Infrastructure',
+      title: 'Missing Infrastructure',
+      text: [
+        'Theres a lack of structured, accessible citizen science initiatives that bridge the gap between professional research and public participation.',
+      ],
+      icon: Circle,
+    },
+  ];
+  return (
+    <Section className='flex flex-col gap-16'>
+      <Tagline text='Why it matters' />
+      <h1>Why are we doing this</h1>
+      <div className='grid gap-8 md:grid-cols-3'>
+        {cardData.map((item) => (
+          <IconTitleTextCard key={item.id} {...item} />
+        ))}
+      </div>
+      <p>
+        These challenges prevent valuable research from happening and limit our collective understanding of marine
+        ecosystems at a time when we need it most.
+      </p>
+    </Section>
+  );
+}
