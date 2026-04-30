c37b15bcb158416bbd921da558ef269cd22b74ea
Kristoffer <kunkristoffer@gmail.com>
2026-04-30 14:31:21 +0200

    Created placeholder component for displaying methods of getting involved in about us get involved section

---
+++ src/components/ui/cards/about/GetInvolvedCard.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { ComponentProps } from 'react';
+
+export interface GetInvolvedData {
+  id: string;
+  title: string;
+  tag: string;
+  text: string;
+  cta: {
+    href: string;
+    label: string;
+  };
+}
+
+type GetInvolvedCardProps = GetInvolvedData & ComponentProps<'div'>;
+
+export function AboutGetInvolvedCard({ title, text, tag, cta }: GetInvolvedCardProps) {
+  return (
+    <div className='flex flex-col gap-8'>
+      <span className='text-primary'>{tag}</span>
+      <h3>{title}</h3>
+      <p className='flex-1'>{text}</p>
+      <Link href={cta.href} className='text-primary hover:underline'>
+        {cta.label}
+      </Link>
+    </div>
+  );
+}
