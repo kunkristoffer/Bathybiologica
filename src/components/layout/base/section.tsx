1d0be7cac847d4a88db3e072f15d564933d46738
Kristoffer <kunkristoffer@gmail.com>
2026-05-01 18:24:42 +0200

    Added default background color to Section component

--- src/components/layout/base/section.tsx
+++ src/components/layout/base/section.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps, ComponentPropsWithoutRef, PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type SectionProps = PropsWithChildren<ComponentPropsWithoutRef<'div'>> & {
   /** Optional tailwind styles for section container */
@@ -10,7 +11,7 @@ type SectionProps = PropsWithChildren<ComponentPropsWithoutRef<'div'>> & {
 export function Section(props: SectionProps) {
   const { children, sectionId, sectionClassName, ...rest } = props;
   return (
-    <section id={sectionId} className={sectionClassName}>
+    <section id={sectionId} className={twMerge('bg-background z-0', sectionClassName)}>
       <div {...rest}>{children}</div>
     </section>
   );
