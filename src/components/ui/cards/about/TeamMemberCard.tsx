b3e9cc5eca94e807d1b37e1ca12117551be762bb
Kristoffer <kunkristoffer@gmail.com>
2026-05-01 15:32:49 +0200

    Removed panel style from about us team member card

--- src/components/ui/cards/about/TeamMemberCard.tsx
+++ src/components/ui/cards/about/TeamMemberCard.tsx
@@ -13,13 +13,15 @@ type TeamMemberCardProps = TeamMember & ComponentProps<'div'>;
 
 export function AboutTeamMemberCard({ name, position, description, image = '' }: TeamMemberCardProps) {
   return (
-    <div className='panel flex flex-col gap-2'>
+    <div className='flex flex-col gap-4'>
       <span className='relative mx-auto w-full aspect-square max-h-48 max-w-48 rounded-full overflow-hidden'>
         <Image src={image} alt={`A profile picture of ${name}`} className='' fill />
       </span>
-      <h3 className='text-primary'>{name}</h3>
-      <p>{position}</p>
-      <p className='text-text-muted'>{description}</p>
+      <h3 className='text-center text-primary'>{name}</h3>
+      <span>
+        <p className='text-center'>{position}</p>
+        <p className='text-center text-text-muted'>{description}</p>
+      </span>
     </div>
   );
 }
