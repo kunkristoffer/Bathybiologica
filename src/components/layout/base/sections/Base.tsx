c6389885e23f07a083808dc123cad22ff15c5f34
Kristoffer <kunkristoffer@gmail.com>
2026-05-05 20:56:47 +0200

    Attempted to make some reusable section container components

---
+++ src/components/layout/base/sections/Base.tsx
@@ -0,0 +1,24 @@
+import { type ComponentProps, type ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface SectionProps extends ComponentProps<'section'> {
+  /** Set the id of this section */
+  id: string;
+  /** All children to be included in this component, will be wrapped in a div */
+  children: ReactNode;
+  /** Optional: Override the tailwind style of the children container */
+  containerClassName?: ComponentProps<'div'>['className'];
+  /** Optional: Add elements to section element before the children container */
+  sectionChildren?: ReactNode;
+}
+
+export function Section({ id, children, className, containerClassName, sectionChildren, ...rest }: SectionProps) {
+  return (
+    <section id={id} className={twMerge('bg-background', className)} {...rest}>
+      {sectionChildren}
+      <div className={twMerge('container mx-auto flex flex-col px-4 md:px-8 py-16 lg:py-24', containerClassName)}>
+        {children}
+      </div>
+    </section>
+  );
+}
---
+++ src/components/layout/base/sections/Parallax.tsx
@@ -0,0 +1,44 @@
+import { type ComponentProps, type ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { Section } from '@/components/layout/base/sections/Base';
+
+interface SectionParallaxProps extends ComponentProps<'section'> {
+  /** Set the id of this section */
+  id: string;
+  /** All children to be included in this component, will be wrapped in a div */
+  children: ReactNode;
+  /** Set the parallaxing image, must exist in `public` folder */
+  image: string;
+  /** Optional: Override the tailwind style of the parallaxing image */
+  imageClassName?: ComponentProps<'span'>['className'];
+  /** Optional: Override the tailwind style of the children container */
+  containerClassName?: ComponentProps<'div'>['className'];
+}
+
+function ParallaxImage({ image, imageClassName }: Pick<SectionParallaxProps, 'image' | 'imageClassName'>) {
+  return (
+    <span
+      className={twMerge(`-z-10 absolute inset-0 bg-cover bg-center bg-fixed`, imageClassName)}
+      style={{ backgroundImage: `url(${image || 'hero-water.jpg'})` }}
+    ></span>
+  );
+}
+
+export function SectionParallax({
+  id,
+  image,
+  imageClassName,
+  children,
+  containerClassName,
+  ...rest
+}: SectionParallaxProps) {
+  return (
+    <Section
+      id={id}
+      children={children}
+      containerClassName={containerClassName}
+      sectionChildren={<ParallaxImage image={image} imageClassName={imageClassName} />}
+      {...rest}
+    />
+  );
+}
