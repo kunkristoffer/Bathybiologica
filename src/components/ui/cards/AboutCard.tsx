92af5d8dba8bef4a5c94b354adfd72ac4e44e66e
Kristoffer <kunkristoffer@gmail.com>
2026-05-06 12:59:19 +0200

    Improvements to about card grid placement

--- src/components/ui/cards/AboutCard.tsx
+++ src/components/ui/cards/AboutCard.tsx
@@ -16,7 +16,7 @@ export function AboutCard({ title, icon: Icon, iconPos = 'inline', className, ch
       className={twMerge(
         `
           grid gap-4 panel h-full
-          ${iconPos === 'top' ? `grid-cols-1 [grid-template-areas:'icon''title''content']` : `grid-cols-[auto_1fr]`}
+          ${iconPos === 'top' ? `auto-rows-[auto_auto_1fr] [grid-template-areas:'icon''title''content']` : `grid-cols-[auto_1fr]`}
           ${iconPos === 'inline' ? `[grid-template-areas:'icon_title''content_content']` : ''}
           ${iconPos === 'left' ? `[grid-template-areas:'icon_title''icon_content']` : ''}
         `,
@@ -25,9 +25,7 @@ export function AboutCard({ title, icon: Icon, iconPos = 'inline', className, ch
       {...rest}
     >
       <Icon className={`[grid-area:icon] self-center justify-self-center text-primary size-8`} />
-      <h3 className={`[grid-area:title] self-center ${iconPos === 'top' ? 'justify-self-center' : ''} text-2xl`}>
-        {title}
-      </h3>
+      <h3 className={`[grid-area:title] self-center ${iconPos === 'top' ? 'text-center' : ''} text-2xl`}>{title}</h3>
       <div className={`[grid-area:content] flex flex-col justify-around gap-4`}>{children}</div>
     </div>
   );
