5c890558cfe6c8bc7c9a497f5c0d046a5708d3c2
Kristoffer <kunkristoffer@gmail.com>
2026-05-06 12:22:07 +0200

    Created reusable card for the about us page

---
+++ src/components/ui/cards/IconTitleCard.tsx
@@ -0,0 +1,34 @@
+import { type LucideIcon } from 'lucide-react';
+import { ComponentProps, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface CardProps extends ComponentProps<'div'> {
+  icon: LucideIcon;
+  title: string;
+  children: ReactNode;
+  /** Override the position of the icon in relation to content */
+  iconPos?: 'top' | 'left' | 'inline';
+}
+
+export function IconTitleCard({ title, icon: Icon, iconPos = 'inline', className, children, ...rest }: CardProps) {
+  return (
+    <div
+      className={twMerge(
+        `
+          grid gap-4 panel
+          ${iconPos === 'top' ? `grid-cols-1 [grid-template-areas:'icon''title''content']` : `grid-cols-[auto_1fr]`}
+          ${iconPos === 'inline' ? `[grid-template-areas:'icon_title''content_content']` : ''}
+          ${iconPos === 'left' ? `[grid-template-areas:'icon_title''icon_content']` : ''}
+        `,
+        className
+      )}
+      {...rest}
+    >
+      <Icon className={`[grid-area:icon] self-center justify-self-center text-primary size-8`} />
+      <h3 className={`[grid-area:title] self-center ${iconPos === 'top' ? 'justify-self-center' : ''} text-2xl`}>
+        {title}
+      </h3>
+      <div className={`[grid-area:content] flex flex-col gap-4`}>{children}</div>
+    </div>
+  );
+}
