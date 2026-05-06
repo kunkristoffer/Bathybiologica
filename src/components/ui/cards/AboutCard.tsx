db07a6690836d600dcf11c213a61bb88d562a7e6
Kristoffer <kunkristoffer@gmail.com>
2026-05-06 13:29:05 +0200

    Extended About card with the tag prop which can be used instead of the Icon

--- src/components/ui/cards/AboutCard.tsx
+++ src/components/ui/cards/AboutCard.tsx
@@ -2,21 +2,34 @@ import { type LucideIcon } from 'lucide-react';
 import { ComponentProps, ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-interface CardProps extends ComponentProps<'div'> {
-  icon: LucideIcon;
+interface CardBaseProps extends ComponentProps<'div'> {
   title: string;
   children: ReactNode;
   /** Override the position of the icon in relation to content */
   iconPos?: 'top' | 'left' | 'inline';
 }
 
-export function AboutCard({ title, icon: Icon, iconPos = 'inline', className, children, ...rest }: CardProps) {
+type CardWithTag = {
+  icon?: never;
+  /** Add a tag above the card title, this is exclusive with icon */
+  tag: string;
+};
+
+type CardWithIcon = {
+  /** Add an icon to the card, this is exclusive with tag */
+  icon: LucideIcon;
+  tag?: never;
+};
+
+type CardProps = CardBaseProps & (CardWithIcon | CardWithTag);
+
+export function AboutCard({ title, icon: Icon, tag, iconPos = 'inline', className, children, ...rest }: CardProps) {
   return (
     <div
       className={twMerge(
         `
-          grid gap-4 panel h-full
-          ${iconPos === 'top' ? `auto-rows-[auto_auto_1fr] [grid-template-areas:'icon''title''content']` : `grid-cols-[auto_1fr]`}
+          grid gap-4 panel h-full auto-rows-[auto_auto_1fr]
+          ${iconPos === 'top' ? `[grid-template-areas:'icon''title''content']` : `grid-cols-[auto_1fr]`}
           ${iconPos === 'inline' ? `[grid-template-areas:'icon_title''content_content']` : ''}
           ${iconPos === 'left' ? `[grid-template-areas:'icon_title''icon_content']` : ''}
         `,
@@ -24,7 +37,8 @@ export function AboutCard({ title, icon: Icon, iconPos = 'inline', className, ch
       )}
       {...rest}
     >
-      <Icon className={`[grid-area:icon] self-center justify-self-center text-primary size-8`} />
+      {tag && !Icon && <span className='[grid-area:icon] text-primary'>{tag}</span>}
+      {Icon && <Icon className={`[grid-area:icon] self-center justify-self-center text-primary size-8`} />}
       <h3 className={`[grid-area:title] self-center ${iconPos === 'top' ? 'text-center' : ''} text-2xl`}>{title}</h3>
       <div className={`[grid-area:content] flex flex-col justify-around gap-4`}>{children}</div>
     </div>
